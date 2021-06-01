a11b52afafc2855a1e14e6c367814bf75593b234
Beto <betoml5@hotmail.com>
2021-05-31 22:56:17 -0500

    Bcrypt service ready

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const SALT_WORK_FACTOR = 10;
 
 const UserModelSchema = new Schema({
   userName: String,
@@ -8,5 +9,23 @@ const UserModelSchema = new Schema({
   password: { type: String, required: true },
   favorites: [],
 });
+
+UserModelSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+UserModelSchema.methods.validatePassword = async function validatePassword(
+  data
+) {
+  return bcrypt.compare(data, this.password);
+};
 
 module.exports = mongoose.model("User", UserModelSchema);
--- routes/User.js
+++ routes/User.js
@@ -3,11 +3,15 @@ const UserService = require("../services/User");
 const router = express.Router();
 const UserServiceLib = new UserService();
 
-router.post("/user", (req, res) => {
+router.post("/user", async (req, res, next) => {
   const { userName, password, email } = req.body;
-  const user = UserServiceLib.createUser(userName, email, password);
+  try {
+    const user = await UserServiceLib.createUser(userName, email, password);
 
-  return res.status(200).send(user);
+    return res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/users", async (req, res) => {
@@ -16,6 +20,7 @@ router.get("/users", async (req, res) => {
   return res.status(200).send({ users });
 });
 
+//Test method. Just avoid it
 router.post("/", (req, res) => {
   return res.status(200).send("Hola mundo");
 });
