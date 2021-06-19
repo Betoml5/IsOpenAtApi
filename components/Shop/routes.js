7035fa6902c5fbce1120568041138ceadc7061ae
Beto <betoml5@hotmail.com>
2021-06-18 22:52:08 -0500

    Delete trycatch from controllers

---
+++ components/Shop/routes.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/all", async (req, res, next) => {
+    try {
+      const shops = await ShopServiceLib.getShops();
+      return res.status(200).send({ shops });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.get("/shop/:id?", async (req, res, next) => {
+    const { id } = req.params;
+  
+    try {
+      const shop = await ShopServiceLib.getById(id);
+      return res.status(200).send({ shop });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.post("/create", async (req, res, next) => {
+    const { name, address, email, phone } = req.body;
+  
+    try {
+      const shop = await ShopServiceLib.create(name, email, address, phone);
+      return res.status(201).send({ shop });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.delete("/remove/:id?", async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const deletedId = await ShopServiceLib.delete(id);
+      return res.status(200).send({ deletedId });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  // TODO -> Ver si realmente necesitamos esta ruta
+  router.patch("/update/:id?", async (req, res, next) => {
+    const { id } = req.params;
+    const { name, email, address } = req.body;
+  
+    try {
+      const shopId = await ShopServiceLib.update(id, name, email, address);
+      return res.status(200).send({ shopId });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.patch("/ispromo/:shopId?", async (req, res, next) => {
+    const { shopId } = req.params;
+    const { promo } = req.body;
+    try {
+      const shopUpdate = await Shop.findOneAndUpdate(
+        shopId,
+        { promo: promo },
+        { new: true }
+      );
+      return res.status(200).send({ shopUpdate });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.patch("/ishot/:shopId?", async (req, res, next) => {
+    const { shopId } = req.params;
+    const { hot } = req.body;
+    try {
+      const shopUpdate = await Shop.findOneAndUpdate(
+        shopId,
+        { hot: hot },
+        { new: true }
+      );
+      return res.status(200).send({ shopUpdate });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.patch("/isopenow/:shopId?", async (req, res, next) => {
+    const { shopId } = req.params;
+    const { openNow } = req.body;
+  
+    try {
+      const shopUpdate = await Shop.findOneAndUpdate(
+        shopId,
+        {
+          openNow: openNow,
+        },
+        { new: true }
+      );
+      return res.status(200).send({ shopUpdate });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.get("/avg/:shopId?", async (req, res, next) => {
+    const { shopId } = req.params;
+    try {
+      const { menu } = await Shop.findById(shopId, { new: true });
+      let allPricesSum = 0;
+      //Sum every price in the menu
+      menu.forEach((comida) => {
+        allPricesSum += comida.price;
+      });
+      // Divide them in the menu length
+      const avgPrice = Math.floor(allPricesSum / menu.length);
+      return res.status(200).send({ avgPrice });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.get("/famous", async (req, res, next) => {
+    try {
+      const shops = await Shop.find({});
+      let starsAndName = [];
+  
+      shops.forEach((element) => {
+        starsAndName.push({
+          name: element.name,
+          stars: element.stars,
+        });
+      });
+      const mostFamous = starsAndName.filter((item) => item.stars >= 3);
+      return res.status(200).send({ mostFamous });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
--- components/User/controller.js
+++ components/User/controller.js
@@ -1,73 +1,48 @@
-const store = require('./store');
+const store = require("./store");
 
-const getUser = async (id) => {
-    try {
-        const user = await store.get(id);
-        return user;
-    } catch (error) {
-        return error;
-    }
-}
+const getUser = (id) => {
+  const user = store.get(id);
+  return user;
+};
 
-const getUsers = async () => {
-    try {
-        const users = await store.getAll();
-        return users
-    } catch (error) {
-        return error;
-    }
-}
+const getUsers = () => {
+  const users = store.getAll();
+  return users;
+};
 
-const createUser = async (username, email, password) => {
-    try {
-        const user = await store.create(username, email, password)
-        return user;
-    } catch (error) {
-        return error;
-    }
-}
+const createUser = (username, email, password) => {
+  const user = store.create(username, email, password);
+  return user;
+};
 
-const deleteUser = async (id) => {
-    try {
-        const user = await store.delete(id);
-        return user;
-    } catch (error) {
-        return error;
-    }
-}
+const deleteUser = (id) => {
+  const user = store.delete(id);
+  return user;
+};
 
-const addFavorite = async (id, shop) => {
-    try {
-        const user = await store.addFav(id, shop);
-        return user
-    } catch (error) {
-        return error;
-    }
-}
+const addFavorite = (id, shop) => {
+  const user = store.addFav(id, shop);
+  return user;
+};
 
-const removeFavorite = async (id, shopIndex) => {
-    try {
-        const user = await store.removeFav(id, shopIndex);
-        user.favorites.splice(shopIndex, 1);
-        user.save({ new: true });
-        return user;
-    } catch (error) {
-        return error;
-    }
-}
+const removeFavorite = (id, shopIndex) => {
+  const user = store.removeFav(id, shopIndex);
+  user.favorites.splice(shopIndex, 1);
+  user.save({ new: true });
+  return user;
+};
 
-const getFavorites = async (id) => {
-    try {
-        const favorite = await store.getFav(id);
-        return favorite
-    } catch (error) {
-        return error;
-    }
-
-}
+const getFavorites = (id) => {
+  const favorite = store.getFav(id);
+  return favorite;
+};
 
 module.exports = {
-    getUser, getUsers, createUser,
-    deleteUser, addFavorite, getFavorites,
-    removeFavorite
-}
+  getUser,
+  getUsers,
+  createUser,
+  deleteUser,
+  addFavorite,
+  getFavorites,
+  removeFavorite,
+};
