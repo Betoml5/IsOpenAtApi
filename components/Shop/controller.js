981a44557e9e9bfa170195a74b18a0a1580c4b59
Beto <betoml5@hotmail.com>
2021-06-19 01:02:47 -0500

    Do Shop Component/controllers/routes/store

--- components/Shop/controller.js
+++ components/Shop/controller.js
@@ -1,6 +1,19 @@
-const store = require('./store');
+const store = require("./store");
 
-const createShop = async (name,
+const createShop = async (
+  name,
+  email,
+  address,
+  phone,
+  openNow,
+  hot,
+  promo,
+  stars,
+  avgPrice,
+  schedule
+) => {
+  const shop = store.create(
+    name,
     email,
     address,
     phone,
@@ -9,48 +22,66 @@ const createShop = async (name,
     promo,
     stars,
     avgPrice,
-    schedule) => {
+    schedule
+  );
 
-    const shop = store.create(name,
-        email,
-        address,
-        phone,
-        openNow,
-        hot,
-        promo,
-        stars,
-        avgPrice,
-        schedule);
-
-    return shop;
-
-}
+  return shop;
+};
 
 const deleteShop = async (id) => {
-    const shop = store.delete(id);
-    return shop;
-}
+  const shop = store.delete(id);
+  return shop;
+};
 
 const getShop = async (id) => {
-    const shop = store.get(id);
-    return shop;
-}
-
+  const shop = store.get(id);
+  return shop;
+};
 
 const getShops = async () => {
-    const shops = store.getAll();
-    return shops;
-}
+  const shops = store.getAll();
+  return shops;
+};
 
 const updateShop = async (id, name, email, address) => {
-    const shop = store.update(id, name, email, address)
-    return shop;
-}
+  const shop = store.update(id, name, email, address);
+  return shop;
+};
+
+const isPromo = async (shopId, promo) => {
+  const shop = await store.promo(shopId, promo);
+  return shop;
+};
+
+const isHot = async (shopId, hot) => {
+  const shop = await store.hot(shopId, promo);
+  return shop;
+};
+
+const openNow = async (shopId, openNow) => {
+  const shop = await store.openNow();
+  return shop;
+};
+
+const getAvg = async (shopId) => {
+  const avg = await store.avg(shopId);
+  return avg;
+};
+
+const famous = async () => {
+  const shops = await store.famous();
+  return shops;
+};
 
 module.exports = {
-    create: createShop,
-    delete: deleteShop,
-    get: getShop,
-    getAll: getShops,
-    update: updateShop
-}
+  create: createShop,
+  delete: deleteShop,
+  get: getShop,
+  getAll: getShops,
+  update: updateShop,
+  promo: isPromo,
+  hot: isHot,
+  openNow,
+  avg: getAvg,
+  famous: famous,
+};
--- components/Shop/store.js
+++ components/Shop/store.js
@@ -1,96 +1,171 @@
-
-const Shop = require('./model')
+const Shop = require("./model");
 
 const createShop = (
-    name,
-    email,
-    address,
-    phone,
-    openNow = false,
-    hot = false,
-    promo = false,
-    stars = 0,
-    avgPrice = 0,
-    schedule = {
-        moday: "",
-        tuesday: "",
-        wednesday: "",
-        thursday: "",
-        friday: "",
-        saturday: "",
-        sunday: "",
-    }) => {
-    try {
-        const newShop = new Shop()
-        newShop.name = name;
-        newShop.email = email;
-        newShop.address = address;
-        newShop.phone = phone;
-        newShop.openNow = openNow;
-        newShop.hot = hot;
-        newShop.promo = promo;
-        newShop.stars = stars;
-        newShop.avgPrice = avgPrice;
-        newShop.schedule = schedule;
-        newShop.save({ new: true });
-        return newShop;
-    } catch (error) {
-        return error;
-    }
-}
+  name,
+  email,
+  address,
+  phone,
+  openNow = false,
+  hot = false,
+  promo = false,
+  stars = 0,
+  avgPrice = 0,
+  schedule = {
+    moday: "",
+    tuesday: "",
+    wednesday: "",
+    thursday: "",
+    friday: "",
+    saturday: "",
+    sunday: "",
+  }
+) => {
+  try {
+    const newShop = new Shop();
+    newShop.name = name;
+    newShop.email = email;
+    newShop.address = address;
+    newShop.phone = phone;
+    newShop.openNow = openNow;
+    newShop.hot = hot;
+    newShop.promo = promo;
+    newShop.stars = stars;
+    newShop.avgPrice = avgPrice;
+    newShop.schedule = schedule;
+    newShop.save({ new: true });
+    return newShop;
+  } catch (error) {
+    return error;
+  }
+};
 
 const deleteShop = async (id) => {
-    try {
-        const deletedId = Shop.findOneAndDelete({ _id: id }, { new: true });
-        return deletedId;
-    } catch (error) {
-        return error;
-    }
-}
+  try {
+    const deletedId = Shop.findOneAndDelete({ _id: id }, { new: true });
+    return deletedId;
+  } catch (error) {
+    return error;
+  }
+};
 
 const getShop = async (id) => {
-    try {
-        const shop = await Shop.findById(id).exec();
-        return shop;
-    } catch (error) {
-        return error;
-    }
-}
+  try {
+    const shop = await Shop.findById(id).exec();
+    return shop;
+  } catch (error) {
+    return error;
+  }
+};
 
 const getShops = async () => {
-    try {
-        const shops = await Shop.find({});
-        return shops;
-    } catch (error) {
-        return error;
-    }
-}
+  try {
+    const shops = await Shop.find({});
+    return shops;
+  } catch (error) {
+    return error;
+  }
+};
 
 const updateShop = async (id, name, email, address) => {
-    try {
-        const shopId = Shop.findByIdAndUpdate(
-            id,
-            {
-                name,
-                email,
-                address,
-            },
-            { new: true }
-        ).exec();
+  try {
+    const shopId = Shop.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+        address,
+      },
+      { new: true }
+    ).exec();
 
-        return shopId;
-    } catch (error) {
-        return error;
-    }
-}
+    return shopId;
+  } catch (error) {
+    return error;
+  }
+};
 
+const isPromo = async (shopId, promo) => {
+  try {
+    const shopUpdate = await Shop.findOneAndUpdate(
+      shopId,
+      { promo: promo },
+      { new: true }
+    );
+    return shopUpdate;
+  } catch (error) {
+    return error;
+  }
+};
+
+const isHot = async (shopId, hot) => {
+  try {
+    const shopUpdate = await Shop.findOneAndUpdate(
+      shopId,
+      { hot: hot },
+      { new: true }
+    );
+    return shopUpdate;
+  } catch (error) {
+    return error;
+  }
+};
+
+const isOpenNow = async (shopId, openNow) => {
+  try {
+    const shopUpdate = await Shop.findOneAndUpdate(
+      shopId,
+      {
+        openNow: openNow,
+      },
+      { new: true }
+    );
+    return shopUpdate;
+  } catch (error) {
+    return error;
+  }
+};
+
+const avgPrice = async (shopId) => {
+  try {
+    const { menu } = await Shop.findById(shopId, { new: true });
+    let allPricesSum = 0;
+    //Sum every price in the menu
+    menu.forEach((comida) => {
+      allPricesSum += comida.price;
+    });
+    // Divide them in the menu length
+    const avgPrice = Math.floor(allPricesSum / menu.length);
+    return avgPrice;
+  } catch (error) {
+    return error;
+  }
+};
+
+const mostFamous = async () => {
+  try {
+    const shops = await Shop.find({});
+    let starsAndName = [];
+    shops.forEach((element) => {
+      starsAndName.push({
+        name: element.name,
+        stars: element.stars,
+      });
+    });
+    return starsAndName;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
-    create: createShop,
-    delete: deleteShop,
-    get: getShop,
-    getAll: getShops,
-    update: updateShop,
-
-
-}
+  create: createShop,
+  delete: deleteShop,
+  get: getShop,
+  getAll: getShops,
+  update: updateShop,
+  promo: isPromo,
+  hot: isHot,
+  openNow: isOpenNow,
+  avg: avgPrice,
+  famous: mostFamous,
+};
